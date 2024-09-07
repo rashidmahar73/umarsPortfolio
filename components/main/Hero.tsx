965016359b87056b18aef391cf2475239384d0cb
rashidmahar73 <rashidmahar73@gmail.com>
2024-09-06 17:11:32 -0700

    video update

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -1,22 +1,23 @@
 import React from "react";
 import HeroContent from "../sub/HeroContent";
-import '../../app/globals.css'
+import "../../app/globals.css";
 
 const Hero = () => {
   return (
-<div className="relative flex flex-col h-screen justify-center items-center text-center" id="home">
+    <div
+      className="relative flex flex-col h-screen justify-center items-center text-center"
+      id="home"
+    >
       <video
         autoPlay
         muted
         loop
-        className="rotate-180 absolute top-[-450px] left-1/2 transform -translate-x-1/2 h-full w-auto z-10 object-cover video-sphere"
+        className="rotate-180 absolute top-[-430px] left-1/2 transform -translate-x-1/2 h-full w-auto z-10  video-sphere"
       >
-        <source src="/sphere.mp4" type="video/mp4" />
+        <source src="/animatedVideo2.mp4" type="video/mp4" />
       </video>
       <HeroContent />
     </div>
-
-
   );
 };
 
